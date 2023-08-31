d84ff12844cea2fc7afa244ac7387e3c92cd5789
Ethan Glover <hello@ethang.email>
2023-08-31 12:08:16 -0500

    Update build.mjs and package.json files
    Added a build script in build.mjs file to automate the process of updating 'peerDependencies' in package.json and calling 'npm publish'. The build script reads data from package.json, gets the latest versions of 'eslint', 'prettier', and 'typescript' from 'dependencies', and updates them in 'peerDependencies'. It also saves the updated package.json back to disk. A 'build' script has also been added to 'scripts' in package.json.
    
    This change ensures that 'peerDependencies' always have the same versions as 'dependencies' before publishing, which can help other projects using this project as a dependency, to have consistent behavior and minimize the risk of running into version-mismatch related issues.

--- build.mjs
+++ build.mjs
@@ -1,3 +1,17 @@
-import { execSync } from 'child_process';
+import fs from 'node:fs'
+
+const packageJson = fs.readFileSync('package.json', { encoding: 'utf8' })
+
+const packageObject = JSON.parse(packageJson)
+
+const peerDependencies = {
+  eslint: packageObject.dependencies.eslint,
+  prettier: packageObject.dependencies.prettier,
+  typescript: packageObject.dependencies.typescript,
+}
+
+packageObject.peerDependencies = peerDependencies
+
+fs.writeFileSync('package.json', JSON.stringify(packageObject, null, 2) + '\n', 'utf8');
 
 execSync('npm publish --access public');

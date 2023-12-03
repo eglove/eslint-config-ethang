03cdc43c3efaeb8437e2bd1386c19790cf97afe8
Ethan Glover <hello@ethang.email>
2023-12-03 12:23:06 -0600

    Remove manual npm publish in favor of automation
    The 'execSync' function previously used for npm publishing in the build.mjs script has been removed. This was done to help automate the publishing process, thereby reducing the need for manual interaction and increasing the speed of the release cycle.

--- build.mjs
+++ build.mjs
@@ -1,5 +1,4 @@
 import { projectBuilder } from '@ethang/project-builder/project-builder.js'
-import { execSync } from 'node:child_process'
 
 await projectBuilder('eslint-config-ethang', 'master', {
   isIgnoringBuild: true,
@@ -7,5 +6,3 @@ await projectBuilder('eslint-config-ethang', 'master', {
   postVersionBumpScripts: ['DEDUPE', 'LINT'],
   ignorePeerDependencies: ['@rushstack/eslint-patch'],
 })
-
-execSync('npm publish --access public')

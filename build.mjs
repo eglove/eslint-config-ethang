ae43047f33426fbc9bc5d0b3bb1b409a19cd25f2
Ethan Glover <hello@ethang.email>
2023-12-04 07:46:45 -0600

    update builder

--- build.mjs
+++ build.mjs
@@ -1,7 +1,7 @@
 import { projectBuilder } from '@ethang/project-builder/project-builder.js'
 
 await projectBuilder('eslint-config-ethang', 'master', {
-  isIgnoringBuild: true,
+  isLibrary: true,
   preVersionBumpScripts: ['UPDATE', 'PRUNE'],
   postVersionBumpScripts: ['DEDUPE', 'LINT'],
   ignorePeerDependencies: ['@rushstack/eslint-patch'],

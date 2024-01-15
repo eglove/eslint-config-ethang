9901f95c9aac7ae3d85f62a78f26e3c224b7d641
Ethan Glover <hello@ethang.email>
2024-01-14 18:10:08 -0600

    require reset

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 /* eslint-disable unicorn/prefer-module,@typescript-eslint/no-var-requires,@typescript-eslint/no-require-imports,@typescript-eslint/no-unsafe-call */
-const eslintrc = require('./.eslintrc');
+const eslintrc = require("./.eslintrc");
+require("@total-typescript/ts-reset");
 
 module.exports = eslintrc;

e910cb47f229391eac0d49e128ca88b67c163d7e
Ethan Glover <hello@ethang.email>
2024-01-14 18:29:06 -0600

    Update ESLint rules and remove '@total-typescript/ts-reset'
    New rules have been added to .eslintrc.js to enforce more type checking in the code. The '@total-typescript/ts-reset' package was removed from index.js, package.json, and pnpm-lock.yaml as it was no longer needed. Overall, these changes aim to improve the quality and robustness of the code base.

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 /* eslint-disable unicorn/prefer-module,@typescript-eslint/no-var-requires,@typescript-eslint/no-require-imports,@typescript-eslint/no-unsafe-call */
 const eslintrc = require("./.eslintrc");
-require("@total-typescript/ts-reset");
 
 module.exports = eslintrc;

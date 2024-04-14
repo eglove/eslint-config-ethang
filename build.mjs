6dbafb31438f6dc988ab340dbbdcf73aa46ce1ab
Ethan Glover <hello@ethang.email>
2024-04-14 13:03:29 -0500

    remove prune

--- build.mjs
+++ build.mjs
@@ -2,7 +2,7 @@ import { projectBuilder } from '@ethang/project-builder/project-builder.js'
 
 await projectBuilder('eslint-config-ethang', 'master', {
   isLibrary: true,
-  preVersionBumpScripts: ['UPDATE', 'PRUNE'],
+  preVersionBumpScripts: ['UPDATE'],
   postVersionBumpScripts: ['DEDUPE', 'LINT'],
   ignorePeerDependencies: ['@rushstack/eslint-patch'],
 })

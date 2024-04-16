e3ca6a831ffe192e0accb420a7702dd45f752b16
Ethan Glover <hello@ethang.email>
2024-04-16 18:30:08 -0500

    remove old

--- build.mjs
+++ build.mjs
@@ -2,22 +2,8 @@
 import { projectBuilder } from "@ethang/project-builder/project-builder.js";
 
 await projectBuilder("eslint-config-ethang", "master", {
-  ignorePeerDependencies: ["@rushstack/eslint-patch"],
   isLibrary: true,
   postVersionBumpScripts: ["DEDUPE", "LINT"],
   preVersionBumpScripts: ["UPDATE"],
   publishDirectory: "dist",
-  tsConfigOverrides: {
-    compilerOptions: {
-      emitDeclarationOnly: true,
-    },
-    include: ["src"],
-  },
-  tsupOptions: {
-    bundle: true,
-    entry: ["index.ts"],
-    format: ["cjs", "esm"],
-    minify: true,
-    outDir: "dist",
-  },
 });

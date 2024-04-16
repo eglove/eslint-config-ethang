e9fd70916e541b5ce848fba7b77d9910049a2655
Ethan Glover <hello@ethang.email>
2024-04-16 18:58:31 -0500

    remove uncesseary

--- build.mjs
+++ build.mjs
@@ -1,9 +1,6 @@
 // @ts-check
 /* eslint-disable @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access */
-import * as fs from "node:fs";
 import { projectBuilder } from "@ethang/project-builder/project-builder.js";
-
-fs.copyFileSync("index.js", "eslint.config.js");
 
 await projectBuilder("eslint-config-ethang", "master", {
   isLibrary: true,

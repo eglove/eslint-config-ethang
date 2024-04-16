38ca85ea20c33e832f17002e5aef41bba52d37fd
Ethan Glover <hello@ethang.email>
2024-04-16 18:36:06 -0500

    create config file

--- build.mjs
+++ build.mjs
@@ -1,6 +1,9 @@
 // @ts-check
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 import { projectBuilder } from "@ethang/project-builder/project-builder.js";
+import * as fs from 'node:fs'
+
+fs.copyFileSync('index.js', 'eslint.config.js')
 
 await projectBuilder("eslint-config-ethang", "master", {
   isLibrary: true,
---
+++ index.js
@@ -0,0 +1,23 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-call */
+import eslint from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
+import eslintPluginUnicorn from "eslint-plugin-unicorn";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  eslint.configs.all,
+  ...tseslint.configs.strictTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  eslintPluginUnicorn.configs["flat/all"],
+  eslintConfigPrettier,
+  eslintPluginPrettier,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+);

391c906e3164f0c39885d6ab8bb0fb70c020c3d9
Ethan Glover <hello@ethang.email>
2024-04-16 18:09:19 -0500

    redo

--- build.mjs
+++ build.mjs
@@ -1,8 +1,23 @@
+// @ts-check
 import { projectBuilder } from '@ethang/project-builder/project-builder.js'
 
 await projectBuilder('eslint-config-ethang', 'master', {
   isLibrary: true,
+  publishDirectory: 'dist',
   preVersionBumpScripts: ['UPDATE'],
   postVersionBumpScripts: ['DEDUPE', 'LINT'],
   ignorePeerDependencies: ['@rushstack/eslint-patch'],
+  tsConfigOverrides: {
+    include: ['src'],
+    compilerOptions: {
+      emitDeclarationOnly: true,
+    },
+  },
+  tsupOptions: {
+    format: ['cjs', 'esm'],
+    minify: true,
+    outDir: 'dist',
+    entry: ['index.ts'],
+    bundle: true,
+  },
 })
--- index.js
+++
@@ -1,4 +0,0 @@
-/* eslint-disable unicorn/prefer-module,@typescript-eslint/no-var-requires,@typescript-eslint/no-require-imports,@typescript-eslint/no-unsafe-call */
-const eslintrc = require("./.eslintrc");
-
-module.exports = eslintrc;

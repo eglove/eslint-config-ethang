59b0deaa78f06524d29cb0772cb7f55eb5859748
Ethan Glover <hello@ethang.email>
2024-04-16 20:51:31 -0500

    rules

--- eslint.config.js
+++ eslint.config.js
@@ -30,7 +30,7 @@ export default tseslint.config(
       "@typescript-eslint/no-import-type-side-effects": "error",
       "@typescript-eslint/no-unused-vars": [
         "error",
-        { args: "all", argsIgnorePattern: "^_" },
+        { args: "all", argsIgnorePattern: "^_$|^_" },
       ],
       "@typescript-eslint/prefer-readonly": "error",
       "@typescript-eslint/prefer-readonly-parameter-types": "error",

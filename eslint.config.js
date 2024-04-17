813ededc5a90b61d8faa1b877ab7538ac598246a
Ethan Glover <hello@ethang.email>
2024-04-16 19:25:31 -0500

    import sort

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@
 import eslint from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
 import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
+import importSort from "eslint-plugin-simple-import-sort";
 import eslintPluginUnicorn from "eslint-plugin-unicorn";
 import tseslint from "typescript-eslint";
 
@@ -19,9 +20,15 @@ export default tseslint.config(
         tsconfigRootDir: import.meta.dirname,
       },
     },
+    plugins: {
+      "simple-import-sort": importSort,
+    },
     rules: {
       "no-undefined": "off",
       "one-var": ["error", "never"],
+      "simple-import-sort/exports": "error",
+      "simple-import-sort/imports": "error",
+      "sort-imports": "off",
       "unicorn/no-null": "off",
     },
   },

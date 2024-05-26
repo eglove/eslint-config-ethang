70e520863141f180847662c3949c00a87b6566e2
Ethan Glover <hello@ethang.email>
2024-05-26 18:46:55 -0500

    update

--- build.mjs
+++ build.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-call, n/no-unpublished-import */
 import { projectBuilder } from "@ethang/project-builder/project-builder.js";
 
 await projectBuilder("eslint-config-ethang", "master", {

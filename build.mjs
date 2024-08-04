6c50cb1fd8b5978d701bdaeff1c05f43ee364ce3
Ethan Glover <hello@ethang.email>
2024-08-04 12:21:10 -0500

    Remove browerslist, this will be built into project builder

--- build.mjs
+++ build.mjs
@@ -1,11 +1,5 @@
 // @ts-check
 import { projectBuilder } from '@ethang/project-builder/project-builder.js'
-import { execSync } from 'node:child_process'
-
-execSync('pnpx update-browserslist-db')
-execSync('git add .')
-execSync('git commit -m "Update BrowsersList')
-execSync('git push')
 
 await projectBuilder('eslint-config-ethang', 'master', {
 	isLibrary: true,

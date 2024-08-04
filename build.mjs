acab3f3df9df984e9bd9ec62b0ec2927b6180a77
Ethan Glover <hello@ethang.email>
2024-08-04 12:13:27 -0500

    Update Commit

--- build.mjs
+++ build.mjs
@@ -2,11 +2,6 @@
 import { projectBuilder } from '@ethang/project-builder/project-builder.js'
 import { execSync } from 'node:child_process'
 
-execSync('pnpx update-browserslist-db')
-execSync('git add .')
-execSync('git commit -m "Update BrowsersList')
-execSync('git push')
-
 await projectBuilder('eslint-config-ethang', 'master', {
 	isLibrary: true,
 	postVersionBumpScripts: ['DEDUPE', 'LINT'],
@@ -15,3 +10,8 @@ await projectBuilder('eslint-config-ethang', 'master', {
 		bundle: true,
 	},
 })
+
+execSync('pnpx update-browserslist-db')
+execSync('git add .')
+execSync('git commit -m "Update BrowsersList')
+execSync('git push')

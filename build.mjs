5ae4eb250a7972469fc6946249548a8227db995a
Ethan Glover <hello@ethang.email>
2024-08-04 12:12:50 -0500

    Update BrowsersList

--- build.mjs
+++ build.mjs
@@ -1,11 +1,17 @@
 // @ts-check
-import { projectBuilder } from "@ethang/project-builder/project-builder.js";
+import { projectBuilder } from '@ethang/project-builder/project-builder.js'
+import { execSync } from 'node:child_process'
 
-await projectBuilder("eslint-config-ethang", "master", {
-  isLibrary: true,
-  postVersionBumpScripts: ["DEDUPE", "LINT"],
-  preVersionBumpScripts: ["UPDATE"],
-  tsupOptions: {
-    bundle: true,
-  },
-});
+execSync('pnpx update-browserslist-db')
+execSync('git add .')
+execSync('git commit -m "Update BrowsersList')
+execSync('git push')
+
+await projectBuilder('eslint-config-ethang', 'master', {
+	isLibrary: true,
+	postVersionBumpScripts: ['DEDUPE', 'LINT'],
+	preVersionBumpScripts: ['UPDATE'],
+	tsupOptions: {
+		bundle: true,
+	},
+})

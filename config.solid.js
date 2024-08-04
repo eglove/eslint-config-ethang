1859e8a375bdc7b13abfe4f095253e65a8f7466d
Ethan Glover <hello@ethang.email>
2024-08-04 18:50:50 -0500

    fix solid plugin

--- config.solid.js
+++ config.solid.js
@@ -7,7 +7,7 @@ export default tseslint.config({
   files: ["**/*.{jsx,tsx}"],
   ignores,
   languageOptions,
-  plugins: [{ solid }],
+  plugins: { solid },
   rules: {
     "solid/components-return-once": "error",
     "solid/event-handlers": "error",

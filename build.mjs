d09006173246e5703b5dfdd5b795c7d46b94ec9d
Ethan <hello@ethang.email>
2024-08-24 12:27:41 -0500

    add prelint

--- build.mjs
+++ build.mjs
@@ -2,9 +2,11 @@
 import { projectBuilder } from "@ethang/project-builder/project-builder.js";
 import { updateRules } from "./build/update-rules.js";
 import { updateReadme } from "./build/update-readme.js";
+import { execSync } from "node:child_process";
 
 updateRules();
 updateReadme();
+execSync("pnpm lint");
 
 await projectBuilder("eslint-config-ethang", "master", {
   isLibrary: true,

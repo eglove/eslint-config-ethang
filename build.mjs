ca45b5ab98470d70d0ec92d8d361fa7e6d4d79c9
Ethan <hello@ethang.email>
2024-08-27 08:00:15 -0500

    move updates to postinstall

--- build.mjs
+++ build.mjs
@@ -7,16 +7,17 @@ import { updateReactRules } from "./build/update-react-rules.js";
 import { updateSolidRules } from "./build/update-solid-rules.js";
 import { updateAstroRules } from "./build/update-astro-rules.js";
 
-updateRules();
-updateReactRules();
-updateSolidRules();
-updateAstroRules();
-updateReadme();
-execSync("pnpm lint");
-
 await projectBuilder("eslint-config-ethang", "master", {
   isLibrary: true,
   scripts: ["UPDATE", "DEDUPE", "LINT"],
+  postInstall: () => {
+    updateRules();
+    updateReactRules();
+    updateSolidRules();
+    updateAstroRules();
+    updateReadme();
+    execSync("pnpm lint");
+  },
   tsupOptions: {
     bundle: true,
   },

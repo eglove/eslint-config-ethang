631fed74faeffdd1e80b1d9bfd73c51aa151a5e8
Ethan <hello@ethang.email>
2024-09-01 01:17:54 -0500

    restore build

--- build.mjs
+++ build.mjs
@@ -3,21 +3,15 @@ import { projectBuilder } from "@ethang/project-builder/project-builder.js";
 import { updateRules } from "./build/update-rules.js";
 import { updateReadme } from "./build/update-readme.js";
 
-await updateRules();
-updateReadme();
-
-// await projectBuilder("eslint-config-ethang", "master", {
-//   isLibrary: true,
-//   scripts: ["UPDATE", "DEDUPE", "LINT"],
-//   postInstall: async () => {
-//     console.log("Updating Rules...");
-//     await updateRules();
-//     await updateReactRules();
-//     updateSolidRules();
-//     updateAstroRules();
-//     updateReadme();
-//   },
-//   tsupOptions: {
-//     bundle: true,
-//   },
-// });
+await projectBuilder("eslint-config-ethang", "master", {
+  isLibrary: true,
+  scripts: ["UPDATE", "DEDUPE", "LINT"],
+  postInstall: async () => {
+    console.log("Updating Rules...");
+    await updateRules();
+    updateReadme();
+  },
+  tsupOptions: {
+    bundle: true,
+  },
+});

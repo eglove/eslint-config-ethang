f372470c51a5fd92f03eea1a8dc9ca29f0a87bf1
Ethan <hello@ethang.email>
2024-09-27 21:55:03 -0500

    move commands

--- build.mjs
+++ build.mjs
@@ -4,12 +4,12 @@ import { execSync } from "node:child_process";
 
 await projectBuilder("eslint-config-ethang", "master", {
   isLibrary: true,
-  scripts: ["bun x taze latest -I -w", "bun lint"],
-  postInstall: async () => {
-    console.log("Updating Rules...");
-    execSync(`pnpx tsx ./build/update-rules.ts`, { stdio: "inherit" });
-    execSync(`pnpx tsx ./build/update-readme.ts`, { stdio: "inherit" });
-  },
+  scripts: [
+    "bun x taze latest -I -w",
+    "bun lint",
+    "bun ./build/update-rules.ts",
+    "bun ./build/update-readme.ts",
+  ],
   tsupOptions: {
     bundle: true,
   },

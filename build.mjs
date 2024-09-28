bfcaa0ac22f0f8392ef8864267fc388c9dbfa75d
Ethan <hello@ethang.email>
2024-09-27 21:53:01 -0500

    update

--- build.mjs
+++ build.mjs
@@ -4,7 +4,7 @@ import { execSync } from "node:child_process";
 
 await projectBuilder("eslint-config-ethang", "master", {
   isLibrary: true,
-  scripts: ["UPDATE", "DEDUPE", "LINT"],
+  scripts: ["bun x taze latest -I -w", "bun linst"],
   postInstall: async () => {
     console.log("Updating Rules...");
     execSync(`pnpx tsx ./build/update-rules.ts`, { stdio: "inherit" });

4a02b77383534ef08819f266aad8c24cef6ae317
Ethan <hello@ethang.email>
2024-10-04 20:09:07 -0500

    v19.10.3

--- eslint.config.js
+++ eslint.config.js
@@ -82,6 +82,7 @@ export default tseslint.config(
               "speculationrules",
               "zustand",
               "vinxi",
+              "datasource",
             ],
           },
         },
